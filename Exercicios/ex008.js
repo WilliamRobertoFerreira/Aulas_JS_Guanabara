d6ecd36a0e1a6f307677b9ee25b0d624f6a46065
William Ferreira <williamroberto.crow@gmail.com>
2024-04-26 15:08:49 -0300

    Feat - Add new commits for Decisions algorithms

---
+++ Exercicios/ex008.js
@@ -0,0 +1,13 @@
+const vel = 78
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log('Sua velocidade ultrapassou o limite permitido. Você foi multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
+
+var pais = 'BRASIL'
+if (pais == 'BRASIL'){
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é estrangeiro!')
+}
